90ebe5f543c1d3f7a509a35b3afdb3c22e982435
Christopher Fofrich <cfofrich@gmail.com>
2020-11-10 19:31:02 -0500

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x <= 100; x++)
+
+                if (x % 15 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (x % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (x % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(x);
+                }
+        }
+    }
+}
